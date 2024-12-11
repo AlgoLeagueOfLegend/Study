927996c8482039ff6121ed088007b46102adc155
seon <sunju701@gmail.com>
2024-12-12 01:47:00 +0900

    feat: add /gitignore

---
+++ seonju/BOJ_10819.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// BOJ_10819 차이를 최대로
+public class BOJ_10819 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
